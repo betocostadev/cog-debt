55be421bf945dced5194a39b975cfea1397d7ddc
betocostadev <robertomarx@gmail.com>
2026-08-06 10:30:34 +0100

    chore: removes console statements and adds error handling to user form

--- src/components/organisms/Users/UserForm.tsx
+++ src/components/organisms/Users/UserForm.tsx
@@ -13,6 +13,9 @@ import { useNavigate } from '@tanstack/react-router'
 import { useGetCompanyDepartments } from '#/hooks/company/useCompany'
 import { DepartmentSelector } from '../Company/DepartmentSelector'
 import type { ICompanyDepartment } from '#/types/company'
+import { JobTitleSelector } from '../Company/JobTitleSelector'
+import { ErrorBoundary } from '#/components/molecules/ErrorBoundary'
+import { toast } from 'sonner'
 
 interface IUserFormProps {
   isEditing: boolean
@@ -36,7 +39,6 @@ export function UserForm({
   const phoneId = useId()
   const cityId = useId()
   const stateId = useId()
-  const jobTitleId = useId() // import user roles T-Area-Functions - company types
   const admDateId = useId()
   const salaryId = useId()
 
@@ -47,12 +49,9 @@ export function UserForm({
     isLoading: isFetchingDepartments,
     error,
   } = useGetCompanyDepartments()
-  console.log('[UserForm - Departments query]')
-  console.log(deptsData)
-  console.log(isFetchingDepartments)
-  console.log(error)
 
   const [isImageDialogOpen, setIsImageDialogOpen] = useState(false)
+  const [hasInitialized, setHasInitialized] = useState(false)
 
   // } = useForm<TUserDataInput>({ // Zod conflicts with React Hook form due to coerce
   const {
@@ -82,9 +81,12 @@ export function UserForm({
   const deptFunctions = selectedDepartment?.functions ?? []
 
   useEffect(() => {
-    if (deptsData?.departments && deptsData.departments.length > 0) {
+    if (
+      deptsData?.departments &&
+      deptsData.departments.length > 0 &&
+      !hasInitialized
+    ) {
       if (userData) {
-        // Populate form with existing user data
         reset({
           id: userData.id,
           status: userData.status,
@@ -103,11 +105,22 @@ export function UserForm({
             .split('T')[0],
           salary: userData.salary,
         })
+        setHasInitialized(true)
       } else if (!isEditing && !currentDepartmentTitle) {
         setValue('department', deptsData.departments[0].title)
+        setValue('jobTitle', deptsData.departments[0].functions[0])
+        setHasInitialized(true)
       }
     }
-  }, [userData, deptsData, isEditing, reset, setValue, currentDepartmentTitle])
+  }, [
+    userData,
+    deptsData,
+    isEditing,
+    reset,
+    setValue,
+    currentDepartmentTitle,
+    hasInitialized,
+  ])
 
   const onFormSubmit = (data: TUserDataInput) => {
     if (onSubmit) {
@@ -123,203 +136,219 @@ export function UserForm({
     }
   }
 
-  return (
-    <form onSubmit={handleSubmit(onFormSubmit)}>
-      <div className="flex justify-between pb-4">
-        <div>
-          <Controller
-            name="image"
-            control={control}
-            render={({ field }) => (
-              <>
-                <div
-                  onClick={() => setIsImageDialogOpen(true)}
-                  className="flex content-center items-center justify-center h-28 w-28 rounded-full overflow-hidden bg-slate-200 cursor-pointer border border-dashed border-slate-400 hover:opacity-90 transition"
-                >
-                  {field.value ? (
-                    <img
-                      aria-disabled={isLoading}
-                      src={field.value}
-                      alt="Avatar Preview"
-                      className="h-full w-full object-cover"
-                    />
-                  ) : (
-                    <span className="text-slate-600 text-xs text-center p-2">
-                      {isLoading ? 'Loading...' : 'Link image'}
-                    </span>
-                  )}
-                </div>
+  if (error) {
+    toast.error(error.message)
+    handleCancel()
+  }
 
-                <ImageLinkDialog
-                  showDialog={isImageDialogOpen}
-                  setShowDialog={setIsImageDialogOpen}
-                  currentImage={field.value}
-                  onImageSelected={(url) => field.onChange(url)}
-                />
-              </>
+  return (
+    <ErrorBoundary>
+      <form onSubmit={handleSubmit(onFormSubmit)}>
+        <div className="flex justify-between pb-4">
+          <div>
+            <Controller
+              name="image"
+              control={control}
+              render={({ field }) => (
+                <>
+                  <div
+                    onClick={() => setIsImageDialogOpen(true)}
+                    className="flex content-center items-center justify-center h-28 w-28 rounded-full overflow-hidden bg-slate-200 cursor-pointer border border-dashed border-slate-400 hover:opacity-90 transition"
+                  >
+                    {field.value ? (
+                      <img
+                        aria-disabled={isLoading}
+                        src={field.value}
+                        alt="Avatar Preview"
+                        className="h-full w-full object-cover"
+                      />
+                    ) : (
+                      <span className="text-slate-600 text-xs text-center p-2">
+                        {isLoading ? 'Loading...' : 'Link image'}
+                      </span>
+                    )}
+                  </div>
+
+                  <ImageLinkDialog
+                    showDialog={isImageDialogOpen}
+                    setShowDialog={setIsImageDialogOpen}
+                    currentImage={field.value}
+                    onImageSelected={(url) => field.onChange(url)}
+                  />
+                </>
+              )}
+            />
+            {errors.image && (
+              <span className="text-red-500 block mt-2">
+                {errors.image.message}
+              </span>
             )}
-          />
-          {errors.image && (
-            <span className="text-red-500 block mt-2">
-              {errors.image.message}
-            </span>
-          )}
+          </div>
+          <div>
+            {isEditing && userId && <p>ID: {userId}</p>}
+            <Controller
+              name="status"
+              control={control}
+              render={({ field }) => (
+                <StatusSelector
+                  isLoading={isLoading || isSubmitting}
+                  selectedStatus={field.value}
+                  onStatusChange={field.onChange}
+                />
+              )}
+            />
+            {errors.status && (
+              <span className="text-red-500 text-sm">
+                {errors.status.message}
+              </span>
+            )}
+          </div>
         </div>
-        <div>
-          {isEditing && userId && <p>ID: {userId}</p>}
+
+        <h3 className="flex y-4 text-lg items-center">
+          <LazyIcon icon={icons.User} iconClassName="mr-4" /> User information
+        </h3>
+
+        <div className="flex w-full sm:flex-col sm:gap-4 lg:flex-row lg:gap-6 mt-4">
+          <InputText
+            className="w-2/4 sm:w-full lg:w-2/4"
+            id={firstNameId}
+            label="First Name"
+            error={errors.firstName?.message}
+            disabled={isLoading || isSubmitting}
+            {...register('firstName')}
+          />
+          <InputText
+            className="w-2/4 sm:w-full lg:w-2/4"
+            id={lastNameId}
+            label="Last Name"
+            error={errors.lastName?.message}
+            disabled={isLoading || isSubmitting}
+            {...register('lastName')}
+          />
+        </div>
+        <div className="flex w-full sm:flex-col sm:gap-4 lg:flex-row lg:gap-6 mt-4">
+          <InputText
+            className="w-2/4 sm:w-full lg:w-2/4"
+            id={usernameId}
+            label="Username"
+            error={errors.username?.message}
+            disabled={isLoading || isSubmitting}
+            {...register('username')}
+          />
+          <InputText
+            className="w-2/4 sm:w-full lg:w-2/4"
+            id={emailId}
+            label="Email"
+            error={errors.email?.message}
+            disabled={isLoading || isSubmitting}
+            {...register('email')}
+          />
+        </div>
+        <div className="flex w-full sm:flex-col sm:gap-4 lg:flex-row lg:gap-6 mt-4 mb-4">
+          <InputText
+            className="w-2/5 sm:w-full lg:w-2/5"
+            id={phoneId}
+            label="Phone"
+            error={errors.phone?.message}
+            disabled={isLoading || isSubmitting}
+            {...register('phone')}
+          />
+          <InputText
+            className="w-2/5 sm:w-full lg:w-2/5"
+            id={cityId}
+            label="City"
+            error={errors.city?.message}
+            disabled={isLoading || isSubmitting}
+            {...register('city')}
+          />
+          <InputText
+            className="w-1/5 sm:w-full lg:w-1/5"
+            id={stateId}
+            label="State"
+            error={errors.state?.message}
+            disabled={isLoading || isSubmitting}
+            {...register('state')}
+          />
+        </div>
+        <h3 className="flex y-4 text-lg items-center">
+          <LazyIcon icon={icons.Building2} iconClassName="mr-4" /> Company
+          information
+        </h3>
+
+        <div className="flex w-full sm:flex-col sm:gap-4 lg:flex-row lg:gap-6 mt-4">
           <Controller
-            name="status"
+            name="department"
             control={control}
             render={({ field }) => (
-              <StatusSelector
-                isLoading={isLoading || isSubmitting}
-                selectedStatus={field.value}
-                onStatusChange={field.onChange}
+              <DepartmentSelector
+                isLoading={isFetchingDepartments || isLoading || isSubmitting}
+                departments={deptsData?.departments}
+                selectedDepartmentTitle={field.value}
+                onDepartmentChange={field.onChange}
               />
             )}
           />
-          {errors.status && (
+          {errors.department && (
             <span className="text-red-500 text-sm">
-              {errors.status.message}
+              {errors.department.message}
+            </span>
+          )}
+          <Controller
+            name="jobTitle"
+            control={control}
+            render={({ field }) => (
+              <JobTitleSelector
+                isLoading={isFetchingDepartments || isLoading || isSubmitting}
+                deptJobTitles={deptFunctions}
+                jobTitle={field.value}
+                onJobTitleChange={field.onChange}
+              />
+            )}
+          />
+          {errors.jobTitle && (
+            <span className="text-red-500 text-sm">
+              {errors.jobTitle.message}
             </span>
           )}
         </div>
-      </div>
-
-      <h3 className="flex y-4 text-lg items-center">
-        <LazyIcon icon={icons.User} iconClassName="mr-4" /> User information
-      </h3>
-
-      <div className="flex w-full sm:flex-col sm:gap-4 lg:flex-row lg:gap-6 mt-4">
-        <InputText
-          className="w-2/4 sm:w-full lg:w-2/4"
-          id={firstNameId}
-          label="First Name"
-          error={errors.firstName?.message}
-          disabled={isLoading || isSubmitting}
-          {...register('firstName')}
-        />
-        <InputText
-          className="w-2/4 sm:w-full lg:w-2/4"
-          id={lastNameId}
-          label="Last Name"
-          error={errors.lastName?.message}
-          disabled={isLoading || isSubmitting}
-          {...register('lastName')}
-        />
-      </div>
-      <div className="flex w-full sm:flex-col sm:gap-4 lg:flex-row lg:gap-6 mt-4">
-        <InputText
-          className="w-2/4 sm:w-full lg:w-2/4"
-          id={usernameId}
-          label="Username"
-          error={errors.username?.message}
-          disabled={isLoading || isSubmitting}
-          {...register('username')}
-        />
-        <InputText
-          className="w-2/4 sm:w-full lg:w-2/4"
-          id={emailId}
-          label="Email"
-          error={errors.email?.message}
-          disabled={isLoading || isSubmitting}
-          {...register('email')}
-        />
-      </div>
-      <div className="flex w-full sm:flex-col sm:gap-4 lg:flex-row lg:gap-6 mt-4 mb-4">
-        <InputText
-          className="w-2/5 sm:w-full lg:w-2/5"
-          id={phoneId}
-          label="Phone"
-          error={errors.phone?.message}
-          disabled={isLoading || isSubmitting}
-          {...register('phone')}
-        />
-        <InputText
-          className="w-2/5 sm:w-full lg:w-2/5"
-          id={cityId}
-          label="City"
-          error={errors.city?.message}
-          disabled={isLoading || isSubmitting}
-          {...register('city')}
-        />
-        <InputText
-          className="w-1/5 sm:w-full lg:w-1/5"
-          id={stateId}
-          label="State"
-          error={errors.state?.message}
-          disabled={isLoading || isSubmitting}
-          {...register('state')}
-        />
-      </div>
-      <h3 className="flex y-4 text-lg items-center">
-        <LazyIcon icon={icons.Building2} iconClassName="mr-4" /> Company
-        information
-      </h3>
-
-      <div className="flex w-full sm:flex-col sm:gap-4 lg:flex-row lg:gap-6 mt-4">
-        <Controller
-          name="department"
-          control={control}
-          render={({ field }) => (
-            <DepartmentSelector
-              isLoading={isFetchingDepartments || isLoading || isSubmitting}
-              departments={deptsData?.departments}
-              selectedDepartmentTitle={field.value}
-              onDepartmentChange={field.onChange}
-            />
-          )}
-        />
-        {errors.department && (
-          <span className="text-red-500 text-sm">
-            {errors.department.message}
-          </span>
-        )}
-        <InputText
-          className="sm:w-full lg:w-2/4"
-          id={jobTitleId}
-          label="Job title"
-          error={errors.jobTitle?.message}
-          disabled={isLoading || isSubmitting}
-          {...register('jobTitle')}
-        />
-      </div>
-      <div className="flex w-full sm:flex-col sm:gap-4 lg:flex-row lg:gap-6 mt-4">
-        <InputText
-          className="sm:w-full lg:w-2/4"
-          id={admDateId}
-          label="Admission date"
-          type="date"
-          error={errors.admissionDate?.message}
-          disabled={isLoading || isSubmitting}
-          {...register('admissionDate')}
-        />
-        <InputText
-          className="sm:w-full lg:w-2/4"
-          id={salaryId}
-          label="Annual salary (€)"
-          type="number"
-          error={errors.salary?.message}
-          disabled={isLoading || isSubmitting}
-          {...register('salary')}
-        />
-      </div>
-      <div className="mt-6 flex justify-end">
-        <BaseButton
-          type="button"
-          variant="secondary"
-          disabled={isLoading || isSubmitting}
-          className="px-4 py-2 mr-2"
-          label={'Cancel'}
-          onClick={handleCancel}
-        />
-        <BaseButton
-          type="submit"
-          disabled={isLoading || isSubmitting}
-          className="px-4 py-2"
-          label={isEditing ? 'Save Changes' : 'Create User'}
-        />
-      </div>
-    </form>
+        <div className="flex w-full sm:flex-col sm:gap-4 lg:flex-row lg:gap-6 mt-4">
+          <InputText
+            className="sm:w-full lg:w-2/4"
+            id={admDateId}
+            label="Admission date"
+            type="date"
+            error={errors.admissionDate?.message}
+            disabled={isLoading || isSubmitting}
+            {...register('admissionDate')}
+          />
+          <InputText
+            className="sm:w-full lg:w-2/4"
+            id={salaryId}
+            label="Annual salary (€)"
+            type="number"
+            error={errors.salary?.message}
+            disabled={isLoading || isSubmitting}
+            {...register('salary')}
+          />
+        </div>
+        <div className="mt-6 flex justify-end">
+          <BaseButton
+            type="button"
+            variant="secondary"
+            disabled={isLoading || isSubmitting}
+            className="px-4 py-2 mr-2"
+            label={'Cancel'}
+            onClick={handleCancel}
+          />
+          <BaseButton
+            type="submit"
+            disabled={isLoading || isSubmitting}
+            className="px-4 py-2"
+            label={isEditing ? 'Save Changes' : 'Create User'}
+          />
+        </div>
+      </form>
+    </ErrorBoundary>
   )
 }
